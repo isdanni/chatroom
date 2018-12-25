645e29041ad5041a267d32c7e9b1613df1fcb303
isdanni <leedanni@gmail.com>
2018-12-26 00:49:32 +0800

    Updates

---
+++ precache-manifest.d0217ab96e92b98d30ecd81bd91c87e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "87491eb3f268aaf6907e564bf01a6ebe",
+    "url": "/chatroom/static/media/back.87491eb3.jpg"
+  },
+  {
+    "revision": "f111fe0f30e2f102384c",
+    "url": "/chatroom/static/js/runtime~main.f111fe0f.js"
+  },
+  {
+    "revision": "05a9584b10c15d10b49d",
+    "url": "/chatroom/static/js/main.05a9584b.chunk.js"
+  },
+  {
+    "revision": "5cb9b55e9859431b45ee",
+    "url": "/chatroom/static/js/1.5cb9b55e.chunk.js"
+  },
+  {
+    "revision": "05a9584b10c15d10b49d",
+    "url": "/chatroom/static/css/main.82d6858a.chunk.css"
+  },
+  {
+    "revision": "dbf0d305d2dcc3cb08b6dedc97c61f17",
+    "url": "/chatroom/index.html"
+  }
+];
